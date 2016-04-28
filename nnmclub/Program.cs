08d3d9516b90dc345916343fdfbd1e3d8f770325
shakshin <rigid.mgn@gmail.com>
2016-04-28 17:59:13 +0500

    work in progress

--- nnmclub/Program.cs
+++ nnmclub/Program.cs
@@ -25,7 +25,7 @@ namespace nnmclub
                     ShowHelp();
                     break;
                 case "topic":
-                    Topic(args);
+                    Topics(args);
                     break;
                 case "folder":
                     Folder(args);
@@ -39,19 +39,116 @@ namespace nnmclub
             }
         }
 
-        static void Topic(string[] args)
+        static void Topics(string[] args)
         {
-
+            if (args.Length == 1)
+            {
+                Topic.List();
+            }
+            else
+            {
+                switch (args[1])
+                {
+                    case "list":
+                        Topic.List();
+                        break;
+                    case "add":
+                        if (args.Length == 2)
+                        {
+                            int id = 0;
+                            try
+                            {
+                                id = int.Parse(args[1]);
+                            } 
+                            catch (Exception ex)
+                            {
+                                System.Console.WriteLine("Wrong topic id provided");
+                                return;
+                            }
+                            Topic.Add(id);
+                        }
+                        else
+                        {
+                            System.Console.WriteLine("Wrong number of arguments");
+                        }
+                        break;
+                    case "delete":
+                        if (args.Length == 2)
+                        {
+                            int id = 0;
+                            try
+                            {
+                                id = int.Parse(args[1]);
+                            } 
+                            catch (Exception ex)
+                            {
+                                System.Console.WriteLine("Wrong topic id provided");
+                                return;
+                            }
+                            Topic.Delete(id);
+                        }
+                        else
+                        {
+                            System.Console.WriteLine("Wrong number of arguments");
+                        }
+                        break;
+                    default:
+                        System.Console.WriteLine("Wrong command: {0}", args[1]);
+                        break;
+                }
+            }
         }
 
         static void Passkey(string[] args)
         {
-
+            if (args.Length == 1)
+            {
+                if (config.Passkey == String.Empty)
+                {
+                    System.Console.WriteLine("Your passkey is not set for now");
+                } 
+                else
+                {
+                    System.Console.WriteLine("Your passkey: {0}", config.Passkey);
+                }
+                return;
+            }
+            else if (args.Length == 2)
+            {
+                config.Passkey = args[1];
+                config.Save();
+                System.Console.WriteLine("Passkey configured");
+            }
+            else
+            {
+                System.Console.WriteLine("Wrong number of arguments");
+            }
         }
 
         static void Folder(string[] args)
         {
-
+            if (args.Length == 1)
+            {
+                if (config.Folder == String.Empty)
+                {
+                    System.Console.WriteLine("Your download folder is not set for now");
+                }
+                else
+                {
+                    System.Console.WriteLine("Download folder: {0}", config.Folder);
+                }
+                return;
+            }
+            else if (args.Length == 2)
+            {
+                config.Folder = args[1];
+                config.Save();
+                System.Console.WriteLine("Download folder configured");
+            }
+            else
+            {
+                System.Console.WriteLine("Wrong number of arguments");
+            }
         }
 
         static void ShowHelp() 
--- nnmclub/Topic.cs
+++ nnmclub/Topic.cs
@@ -6,7 +6,72 @@ using System.Threading.Tasks;
 
 namespace nnmclub
 {
+    [Serializable]
     public class Topic
     {
+        [NonSerialized]
+        private int _id;
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        [NonSerialized]
+        private String _title;
+
+        public String Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+
+        public static void List()
+        {
+
+        }
+
+        public static void Add(int id)
+        {
+            Config config = Config.Get();
+            foreach (Topic topic in config.Topics)
+            {
+                if (topic.Id == id)
+                {
+                    System.Console.WriteLine("Topic with id {0} is already in list", id);
+                    return;
+                }
+            }
+            Topic t = new Topic();
+            t.Id = id;
+            config.Topics.Add(t);
+            System.Console.WriteLine("Topic with id {0} was added to list", id);
+            config.Save();
+        }
+
+        public static void Delete(int id)
+        {
+            Config config = Config.Get();
+            Topic t = null;
+            foreach (Topic topic in config.Topics)
+            {
+                if (topic.Id == id)
+                {
+                    t = topic;
+                    break;
+                }
+            }
+            if (t == null)
+            {
+                System.Console.WriteLine("Topic with id {0} is not in list", id);
+            }
+            else
+            {
+                config.Topics.Remove(t);
+                System.Console.WriteLine("Topic with id {0} was reoved from list", id);
+                config.Save();
+            }
+        }
     }
 }
