5d3018ac966a3620bc7a4fb3942e7ec71bca6a14
shakshin <rigid.mgn@gmail.com>
2016-04-28 16:14:31 +0500

    test

--- nnmclub/Program.cs
+++ nnmclub/Program.cs
@@ -10,6 +10,22 @@ namespace nnmclub
     {
         static void Main(string[] args)
         {
+            if (args.Length == 0) {
+                ShowHelp();
+                return;
+            }
+            switch (args[0].ToLower())
+            {
+                case "help":
+                    ShowHelp();
+                    break;
+                default:
+                    System.Console.WriteLine(String.Format("Wrong command: {0}", args[0]));
+                    break;
+            }
+        }
+
+        static void ShowHelp() { 
         }
     }
 }
